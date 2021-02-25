25ed2076611ea3651daa59770809e1e27df73a56
Jasmeet Rangar <jasmeetsingr@gmail.com>
2021-02-25 19:52:06 +0000

    Document ready refactor, functions definitions outside

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,3 +1,55 @@
+
+$(document).ready(function () {
+  $.ajax('/tweets/', {
+    method: 'GET'
+  }).then((response) => {
+    renderTweets(response);
+  })
+$('.toggleButton').on('click',navToggle);
+  $('.tweetForm').on('submit', tweetSubmit);           
+})
+
+const renderTweets = function (tweets) {
+  $('.tweetContainer').html("");
+  // loops through tweets
+  const sortedTweets = tweets.sort((a, b) => b.created_at - a.created_at);
+
+  for (let tweet of sortedTweets) {
+    // calls createTweetElement for each tweet
+    let $tweet = createTweetElement(tweet);
+    // takes return value and appends it to the tweets container
+    $(".tweetContainer").append($tweet);
+  }
+}  
+const createTweetElement = function (tweet) {
+  const name = tweet.user.name;
+  const handle = tweet.user.handle;
+  const avatarUrl = tweet.user.avatars;
+  const content = tweet.content.text;
+  const timeStamp = new Date(tweet.created_at).getTime();
+  const today = new Date().getTime();
+  const time = Math.floor((today - timeStamp) / 1000 / 60 / 60 / 24);
+  let singleTweet = `
+  <article>
+<header>
+          <div class="headerLeft">
+          <img class="tweetAvatar" src="${avatarUrl}">
+          <div>${name}</div>
+          </div>
+          <div class="handle">${handle}</div>
+          </header>
+          <div class="single-tweet__rowTwo">
+          <span class="tweet">${content}</span>
+          <footer>
+          <div> ${time} days ago </div>
+          <div>🏴 📟 ❤</div>
+          </footer>
+          </article>
+          `;
+          
+          return singleTweet;
+        }
+
 const navToggle = function (){
   $('.new-tweet').slideToggle();
   $('.new-tweet').focus();
@@ -20,66 +72,13 @@ const tweetSubmit = function (event) {
       url: "/tweets/",
       data: content
     }).done(() => {
+      //clear all tweets
+
       $.ajax('/tweets/', {
         method: 'GET'
       }).then((response) => {
-        renderTweets(response.slice(response.length - 1))
+        renderTweets(response);
       })
     });
   }
 }
-
-$(document).ready(function () {
-  $.ajax('/tweets/', {
-    method: 'GET'
-  }).then((response) => {
-    renderTweets(response);
-  })
-$('.toggleButton').on('click',navToggle);
-  $('.tweetForm').on('submit', tweetSubmit);
-
-
-  const renderTweets = function (tweets) {
-    // loops through tweets
-    //const sortedTweets = tweets.sort((a, b) => b.created_at - a.created_at);
-
-    for (let tweet of tweets) {
-      // calls createTweetElement for each tweet
-      let $tweet = createTweetElement(tweet);
-      // takes return value and appends it to the tweets container
-      $(".tweetContainer").append($tweet);
-    }
-  }
-
-
-
-  const createTweetElement = function (tweet) {
-    const name = tweet.user.name;
-    const handle = tweet.user.handle;
-    const avatarUrl = tweet.user.avatars;
-    const content = tweet.content.text;
-    const timeStamp = new Date(tweet.created_at).getTime();
-    const today = new Date().getTime();
-    const time = Math.floor((today - timeStamp) / 1000 / 60 / 60 / 24);
-    let singleTweet = `
-  <article>
-  <header>
-            <div class="headerLeft">
-            <img class="tweetAvatar" src="${avatarUrl}">
-            <div>${name}</div>
-            </div>
-            <div class="handle">${handle}</div>
-            </header>
-            <div class="single-tweet__rowTwo">
-            <span class="tweet">${content}</span>
-            <footer>
-            <div> ${time} days ago </div>
-            <div>🏴 📟 ❤</div>
-            </footer>
-            </article>
-            `;
-
-    return singleTweet;
-  }
-
-})
